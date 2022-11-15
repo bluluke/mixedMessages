bfac42368987773315a36d74621f2972ece12b97
Luke Ford <lukeford@lukes-imac.home>
2022-11-15 12:26:36 +0000

    I want to see if the gitShow command has the same result twice.

--- mixedMessages.js
+++ mixedMessages.js
@@ -2,3 +2,4 @@
 //This is more practice
 //This is even more practice for my second commit
 //Thus is practice for a commit which won't go to the main branch initially! 
+//I am writing more at 12:24 to see if the git show head has the same result a second time.
