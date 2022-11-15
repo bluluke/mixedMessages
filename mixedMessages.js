56d29ee84d319500cabead9720e123b9e0803f84
Luke Ford <bluluke@hotmail.com>
2022-11-15 15:53:33 +0000

    Another commit from VS2

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,3 +3,5 @@
 //This is even more practice for my second commit
 //Thus is practice for a commit which won't go to the main branch initially! 
 //I am writing more at 12:24 to see if the git show head has the same result a second time.
+//Everything I am writing is completely experimental. 
+//This is a change to see if the staging enacted on VS2 is reflected in the terminal.
